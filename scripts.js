a009094bec847f95b976933119f5499ac9623084
Trevin Madron <tredmad@email.ecok.edu>
2022-11-13 03:50:55 -0600

    Added auto data saturation script for the home page and new UI and CSS elements for items.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(function(){
+$( function(){
     let shoppingList = [];
     let resultList =[];
     let products = [
@@ -7,182 +7,208 @@ $(function(){
             "dept" : "Produce",
             "price" : 1.25,
             "qty" : "In Stock",
-            "img" : "imgs/apple.jpg"
+            "img" : "imgs/apple.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Bananas",
             "dept" : "Produce",
             "price" : 0.69,
             "qty" : "In Stock",
-            "img" : "imgs/banana.jpg"
+            "img" : "imgs/banana.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Carrots",
             "dept" : "Produce",
             "price" : 1.55,
             "qty" : "In Stock",
-            "img" : "imgs/carrots.jpg"
+            "img" : "imgs/carrots.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Broccoli",
             "dept" : "Produce",
             "price" : 4.99,
             "qty" : "In Stock",
-            "img" : "imgs/broc.jpg"
+            "img" : "imgs/broc.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hiland Whole Milk",
             "dept" : "Dairy",
             "price" : 4.25,
             "qty" : 30,
-            "img" : "imgs/milk.jpg"
+            "img" : "imgs/milk.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hiland 2% Milk",
             "dept" : "Dairy",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/2milk.jpg"
+            "img" : "imgs/2milk.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Nesquik Chocolate Milk - Single Serving",
             "dept" : "Dairy",
             "price" : 1.55,
             "qty" : 20,
-            "img" : "imgs/nes.jpg"
+            "img" : "imgs/nes.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Farm Fresh Eggs",
             "dept" : "Dairy",
             "price" : 3.25,
             "qty" : 20,
-            "img" : "imgs/eggs.jpg"
+            "img" : "imgs/eggs.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Digiorno Frozen Pizza",
             "dept" : "Frozen",
             "price" : 6.99,
             "qty" : 20,
-            "img" : "imgs/dig.jpg"
+            "img" : "imgs/dig.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Ben and Jerry's Ice Cream",
             "dept" : "Frozen",
             "price" : 5.25,
             "qty" : 20,
-            "img" : "imgs/ben.jpg"
+            "img" : "imgs/ben.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Gordon's Fish Fillets",
             "dept" : "Frozen",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/gordons.jpg"
+            "img" : "imgs/gordons.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Bird's Eye Frozen Broccoli",
             "dept" : "Frozen",
             "price" : 2.15,
             "qty" : 20,
-            "img" : "imgs/frozbroc.jpg"
+            "img" : "imgs/frozbroc.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Sweet Baby Ray's Barbecue Sauce - BBQ",
             "dept" : "Dry Goods",
             "price" : 3.25,
             "qty" : 20,
-            "img" : "imgs/bbq.jpg"
+            "img" : "imgs/bbq.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Ranch Style Canned Beans",
             "dept" : "Dry Goods",
             "price" : 1.25,
             "qty" : 20,
-            "img" : "imgs/ranch.jpg"
+            "img" : "imgs/ranch.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hidden Valley Ranch",
             "dept" : "Dry Goods",
             "price" : 3.75,
             "qty" : 20,
-            "img" : "imgs/hvranch.jpg"
+            "img" : "imgs/hvranch.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Prego Spaghetti Sauce",
             "dept" : "Dry Goods",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/prego.jpg"
+            "img" : "imgs/prego.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Mahatma Grain Rice",
             "dept" : "Dry Goods",
             "price" : 2.85,
             "qty" : 20,
-            "img" : "imgs/rice.jpg"
+            "img" : "imgs/rice.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Barilla Spaghetti Noodles",
             "dept" : "Dry Goods",
             "price" : 1.75,
             "qty" : 20,
-            "img" : "imgs/spa.jpg"
+            "img" : "imgs/spa.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Dawn Dish Soap",
             "dept" : "Home Goods",
             "price" : 3.65,
             "qty" : 20,
-            "img" : "imgs/dawn.jpg"
+            "img" : "imgs/dawn.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Charmin Extra Soft Toilet Paper",
             "dept" : "Home Goods",
             "price" : 4.75,
             "qty" : 20,
-            "img" : "imgs/charmin.jpg"
+            "img" : "imgs/charmin.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Snuggle Dryer Sheets",
             "dept" : "Home Goods",
             "price" : 4.00,
             "qty" : 20,
-            "img" : "imgs/snug.jpg"
+            "img" : "imgs/snug.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Tide Pods",
             "dept" : "Home Goods",
             "price" : 4.99,
             "qty" : 20,
-            "img" : "imgs/tide.jpg"
+            "img" : "imgs/tide.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Lean Ground Beef",
             "dept" : "Meat",
             "price" : 3.00,
             "qty" : "In Stock",
-            "img" : "imgs/ground.jpg"
+            "img" : "imgs/ground.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Ham",
             "dept" : "Meat",
             "price" : 4.00,
             "qty" : "In Stock",
-            "img" : "imgs/ham.jpg"
+            "img" : "imgs/ham.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Turkey",
             "dept" : "Meat",
             "price" : 6.00,
             "qty" : "In Stock",
-            "img" : "imgs/turkey.jpg"
+            "img" : "imgs/turkey.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Pork Chop",
             "dept" : "Meat",
             "price" : 4.25,
             "qty" : "In Stock",
-            "img" : "imgs/porkchop.jpg"
+            "img" : "imgs/porkchop.jpg",
+            "trending" : "false"
         }
         
     ];
@@ -197,7 +223,58 @@ $(function(){
             "name": "John Smith"
         }
     ]
-    //Product Search Functionality
+
+    // --- Home - "All Items" section fill - Displays all in-stock items on home page
+
+    $(function addHomeScreenItems(){
+        $("#trendingitemslist").empty();
+        $("#itemslist").empty();
+        let j = 0;
+        let noOfTrendingItems = 0;
+        for(let i in products){
+            if (products[i].qty != 0) {
+                $("#itemslist").append(
+                    "<div id='prodDisp'>" +
+                        "<div id='picWrap'>" +
+                            "<img id='prodImg' src='" + products[i].img + "'>" + 
+                        "</div>" +
+                        '<div id=prodInfo">' +
+                            "<p id='prodName'>" + products[i].name + "</p>" +
+                            "<p id='prodDept'>" +products[i].dept + "</p>" +
+                            "<p id='prodPric'>$" + products[i].price + "</p>" +
+                            "<button id='addList"+j+"' value='' ><i class='fa fa-cart-plus'></i></button>" +
+                        "</div>" +
+                    "</div>"
+                    );
+
+                    // Adds items to trending items list - no more than 5
+                    if (products[i].trending == "true" && noOfTrendingItems != 6) {
+                        $("#trendingitemslist").append(
+                            "<div id='prodDisp'>" +
+                                "<div id='picWrap'>" +
+                                    "<img id='prodImg' src='" + products[i].img + "'>" + 
+                                "</div>" +
+                                '<div id=prodInfo">' +
+                                    "<p id='prodName'>" + products[i].name + "</p>" +
+                                    "<p id='prodDept'>" +products[i].dept + "</p>" +
+                                    "<p id='prodPric'>$" + products[i].price + "</p>" +
+                                    "<button id='addList"+j+"' value='' ><i class='fa fa-cart-plus'></i></button>" +
+                                "</div>" +
+                            "</div>"
+                            );
+                            noOfTrendingItems++;
+                    }
+                    //adds button that pushes that item to shopping list
+                $('#addList' + j).on('click', event => {
+                    let n = products[i].name;
+                    shoppingList.push(n); 
+                })
+            }
+        }
+        j++;
+    });
+
+    // --- Product Search - Searches for product
     $("#sub").on("click", function(){
         $("#result").empty();
         let search = $("#search").val();
@@ -266,7 +343,7 @@ $(function(){
   
 
 
-//show shopping list array
+// ---- Show Shopping List Array
   $("#showList").on("click",function(){
     $("#shopList").empty();
     for(let i of shoppingList){
