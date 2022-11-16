361a0a4b0c663be5c9adeefa24298f4d5ad4c65c
Trevin Madron <115761468+TrevinMadron@users.noreply.github.com>
2022-11-15 23:50:57 -0600

    Updated products array to include "trending" field

--- scripts.js
+++ scripts.js
@@ -7,184 +7,209 @@ $(function(){
             "dept" : "Produce",
             "price" : 1.25,
             "qty" : "In Stock",
-            "img" : "imgs/apple.jpg"
+            "img" : "imgs/apple.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Bananas",
             "dept" : "Produce",
             "price" : 0.69,
             "qty" : "In Stock",
-            "img" : "imgs/banana.jpg"
+            "img" : "imgs/banana.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Carrots",
             "dept" : "Produce",
             "price" : 1.55,
             "qty" : "In Stock",
-            "img" : "imgs/carrots.jpg"
+            "img" : "imgs/carrots.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Broccoli",
             "dept" : "Produce",
             "price" : 4.99,
             "qty" : "In Stock",
-            "img" : "imgs/broc.jpg"
+            "img" : "imgs/broc.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hiland Whole Milk",
             "dept" : "Dairy",
             "price" : 4.25,
             "qty" : 30,
-            "img" : "imgs/milk.jpg"
+            "img" : "imgs/milk.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hiland 2% Milk",
             "dept" : "Dairy",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/2milk.jpg"
+            "img" : "imgs/2milk.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Nesquik Chocolate Milk - Single Serving",
             "dept" : "Dairy",
             "price" : 1.55,
             "qty" : 20,
-            "img" : "imgs/nes.jpg"
+            "img" : "imgs/nes.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Farm Fresh Eggs",
             "dept" : "Dairy",
             "price" : 3.25,
             "qty" : 20,
-            "img" : "imgs/eggs.jpg"
+            "img" : "imgs/eggs.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Digiorno Frozen Pizza",
             "dept" : "Frozen",
             "price" : 6.99,
             "qty" : 20,
-            "img" : "imgs/dig.jpg"
+            "img" : "imgs/dig.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Ben and Jerry's Ice Cream",
             "dept" : "Frozen",
             "price" : 5.25,
             "qty" : 20,
-            "img" : "imgs/ben.jpg"
+            "img" : "imgs/ben.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Gordon's Fish Fillets",
             "dept" : "Frozen",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/gordons.jpg"
+            "img" : "imgs/gordons.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Bird's Eye Frozen Broccoli",
             "dept" : "Frozen",
             "price" : 2.15,
             "qty" : 20,
-            "img" : "imgs/frozbroc.jpg"
+            "img" : "imgs/frozbroc.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Sweet Baby Ray's Barbecue Sauce - BBQ",
             "dept" : "Dry Goods",
             "price" : 3.25,
             "qty" : 20,
-            "img" : "imgs/bbq.jpg"
+            "img" : "imgs/bbq.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Ranch Style Canned Beans",
             "dept" : "Dry Goods",
             "price" : 1.25,
             "qty" : 20,
-            "img" : "imgs/ranch.jpg"
+            "img" : "imgs/ranch.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Hidden Valley Ranch",
             "dept" : "Dry Goods",
             "price" : 3.75,
             "qty" : 20,
-            "img" : "imgs/hvranch.jpg"
+            "img" : "imgs/hvranch.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Prego Spaghetti Sauce",
             "dept" : "Dry Goods",
             "price" : 4.25,
             "qty" : 20,
-            "img" : "imgs/prego.jpg"
+            "img" : "imgs/prego.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Mahatma Grain Rice",
             "dept" : "Dry Goods",
             "price" : 2.85,
             "qty" : 20,
-            "img" : "imgs/rice.jpg"
+            "img" : "imgs/rice.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Barilla Spaghetti Noodles",
             "dept" : "Dry Goods",
             "price" : 1.75,
             "qty" : 20,
-            "img" : "imgs/spa.jpg"
+            "img" : "imgs/spa.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Dawn Dish Soap",
             "dept" : "Home Goods",
             "price" : 3.65,
             "qty" : 20,
-            "img" : "imgs/dawn.jpg"
+            "img" : "imgs/dawn.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Charmin Extra Soft Toilet Paper",
             "dept" : "Home Goods",
             "price" : 4.75,
             "qty" : 20,
-            "img" : "imgs/charmin.jpg"
+            "img" : "imgs/charmin.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Snuggle Dryer Sheets",
             "dept" : "Home Goods",
             "price" : 4.00,
             "qty" : 20,
-            "img" : "imgs/snug.jpg"
+            "img" : "imgs/snug.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Tide Pods",
             "dept" : "Home Goods",
             "price" : 4.99,
             "qty" : 20,
-            "img" : "imgs/tide.jpg"
+            "img" : "imgs/tide.jpg",
+            "trending" : "false"
         },
         {
             "name" : "Lean Ground Beef",
             "dept" : "Meat",
             "price" : 3.00,
             "qty" : "In Stock",
-            "img" : "imgs/ground.jpg"
+            "img" : "imgs/ground.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Ham",
             "dept" : "Meat",
             "price" : 4.00,
             "qty" : "In Stock",
-            "img" : "imgs/ham.jpg"
+            "img" : "imgs/ham.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Turkey",
             "dept" : "Meat",
             "price" : 6.00,
             "qty" : "In Stock",
-            "img" : "imgs/turkey.jpg"
+            "img" : "imgs/turkey.jpg",
+            "trending" : "true"
         },
         {
             "name" : "Pork Chop",
             "dept" : "Meat",
             "price" : 4.25,
             "qty" : "In Stock",
-            "img" : "imgs/porkchop.jpg"
+            "img" : "imgs/porkchop.jpg",
+            "trending" : "false"
         }
-        
     ];
     
     let quotes = [
